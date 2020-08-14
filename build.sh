7586ed4ae37ce564afc4a811299df19d2e62ed35
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 18:12:57 -0500

    download bootjdk

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 sudo apt-get install aptitude -y
-sudo aptitude install build-essential libcups2-dev openjdk-14-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev autoconf -y
+sudo aptitude install build-essential libcups2-dev openjdk-11-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev autoconf -y
 git submodule update --init
-cd jdk11u
-wget https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
+wget -N https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz
+tar -xf OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz
+wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
-bash configure --with-jvm-variants=server --with-jvm-features=link-time-opt --enable-ccache --with-jtreg=./jtreg/ \
+cd jdk11u
+bash configure --with-jvm-variants=server --with-jvm-features=link-time-opt --enable-ccache --with-boot-jdk=..//jdk-11.0.8+10/ --with-jtreg=..//jtreg/ \
 --with-extra-cflags='-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -w' \
 --with-extra-cxxflags='-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -w'
 make clean
