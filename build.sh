1597ff40cedcb904aab16f0bffd0a3ef51a324b6
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 16:25:19 -0500

    add depend

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt-get install aptitude
-sudo aptitude install build-essential libcups2-dev openjdk-14-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev autoconf
+sudo aptitude install build-essential libcups2-dev openjdk-14-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev autoconf
 git submodule update --init
 cd jdk15
 bash configure --with-jvm-variants=server --with-jvm-features=link-time-opt \
