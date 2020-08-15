3986f104469659be352788aa7b4c3e37fd3cbfde
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 22:23:06 -0500

    fix

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ tar -xf labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
 wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
 cd labs-openjdk-11
-python build_labsjdk.py --jvmci-version=19.3 --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
+python build_labsjdk.py --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
 --configure-option="--with-extra-cxxflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated" \
 --configure-option="--with-extra-cflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated" \
 --configure-option=--with-jvm-features=link-time-opt --configure-option=--enable-ccache --configure-option=--with-jtreg=..//jtreg/
