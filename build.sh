7ce16a324ea48885bc5a098042f746144745a6f0
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 22:54:04 -0500

    try older build

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 sudo apt-get update
 sudo apt-get install aptitude -y
 sudo aptitude update
-sudo aptitude install build-essential libcups2-dev openjdk-11-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev python3 autoconf -y
+sudo aptitude install build-essential libcups2-dev libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev python3 ccache autoconf -y
 git submodule update --init
 wget -N https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-19.3-b04/labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
 tar -xf labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
 wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
 cd labs-openjdk-11
-git pull
 git reset origin/release/jvmci/19.3
 \cp ..//build_labsjdk.py build_labsjdk.py
 python build_labsjdk.py --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
