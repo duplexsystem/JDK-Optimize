a88680766daaf35aa7431dc8dd8b4c9d026b1e21
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 22:55:39 -0500

    tmp

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ tar -xf labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
 wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
 cd labs-openjdk-11
-git reset origin/release/jvmci/19.3
 \cp ..//build_labsjdk.py build_labsjdk.py
 python build_labsjdk.py --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
 --configure-option="--with-extra-cxxflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated" \
