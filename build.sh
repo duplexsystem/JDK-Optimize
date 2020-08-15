848c620d183b7c03043d3ea078dbd65e8d399795
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 22:19:02 -0500

    use 19.3 and use labsjdk as boot jdk

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ sudo apt-get install aptitude -y
 sudo aptitude update
 sudo aptitude install build-essential libcups2-dev openjdk-11-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev python3 autoconf -y
 git submodule update --init
-wget -N https://github.com/AdoptOpenJDK/openjdk10-binaries/releases/download/jdk-10.0.2%2B13.1/OpenJDK10U-jdk_x64_linux_hotspot_10.0.2_13.tar.gz
-tar -xf OpenJDK10U-jdk_x64_linux_hotspot_10.0.2_13.tar.gz
+wget -N https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-19.3-b04/labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
+tar -xf labsjdk-ce-11.0.5+10-jvmci-19.3-b04-fastdebug-linux-amd64.tar.gz
 wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
 cd labs-openjdk-11
-python build_labsjdk.py --configure-option=--disable-warnings-as-errors --boot-jdk=..//jdk-10.0.2+13/ \
+python build_labsjdk.py --jvmci-version=19.3 --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
 --configure-option="--with-extra-cxxflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated -Wno-errors" \
 --configure-option="--with-extra-cflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated -Wno-errors" \
 --configure-option=--with-jvm-features=link-time-opt --configure-option=--enable-ccache --configure-option=--with-jtreg=..//jtreg/
