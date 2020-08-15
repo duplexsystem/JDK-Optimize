9ff7c4b612cea0082bb9c3a1e65a1d538abf71a8
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 22:19:33 -0500

    remove no errors

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBu
 tar -xf jtreg-5.1-b01.tar.gz
 cd labs-openjdk-11
 python build_labsjdk.py --jvmci-version=19.3 --configure-option=--disable-warnings-as-errors --boot-jdk=..//labsjdk-ce-11.0.5-jvmci-19.3-b04-fastdebug/ \
---configure-option="--with-extra-cxxflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated -Wno-errors" \
---configure-option="--with-extra-cflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated -Wno-errors" \
+--configure-option="--with-extra-cxxflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated" \
+--configure-option="--with-extra-cflags=-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Wno-deprecated" \
 --configure-option=--with-jvm-features=link-time-opt --configure-option=--enable-ccache --configure-option=--with-jtreg=..//jtreg/
 ./build/*/images/jdk/bin/java -version
 make run-test-tier1
