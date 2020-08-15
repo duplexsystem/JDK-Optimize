0c36ffc758444abaf1e720e1c1927b60d0c6194a
Bud Gidiere <sgidiere@gmail.com>
2020-08-14 20:47:11 -0500

    Use labs openjdk for graal

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
+sudo apt-get update
 sudo apt-get install aptitude -y
-sudo aptitude install build-essential libcups2-dev openjdk-11-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev autoconf -y
+sudo aptitude update
+sudo aptitude install build-essential libcups2-dev openjdk-11-jdk libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev libasound2-dev libfontconfig1-dev python3 autoconf -y
 git submodule update --init
 wget -N https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz
 tar -xf OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz
 wget -N https://ci.adoptopenjdk.net/view/Dependencies/job/jtreg/lastSuccessfulBuild/artifact/jtreg-5.1-b01.tar.gz
 tar -xf jtreg-5.1-b01.tar.gz
-cd jdk11u
-bash configure --with-jvm-variants=server --with-jvm-features=link-time-opt --enable-ccache --with-boot-jdk=..//jdk-11.0.8+10/ --with-jtreg=..//jtreg/ \
---with-extra-cflags='-w' \
---with-extra-cxxflags='-w'
+cd labs-openjdk-11
 make clean
+python build_labsjdk.py --configure-option=--disable-warnings-as-errors \
+--configure-option=--with-extra-cxxflags=-'-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Werror' \
+--configure-option=--with-extra-cflags='-Ofast -march=native -mtune=native -funroll-loops -fomit-frame-pointer -pipe -fstack-protector-strong --param=ssp-buffer-size=4 -fno-plt -fopenmp -pthread -Werror' \
+--configure-option=--with-jvm-features=link-time-opt --configure-option=--enable-ccache --configure-option=--with-boot-jdk=..//jdk-11.0.8+10/ --configure-option=--with-jtreg=..//jtreg/
 make images
 ./build/*/images/jdk/bin/java -version
 make run-test-tier1
